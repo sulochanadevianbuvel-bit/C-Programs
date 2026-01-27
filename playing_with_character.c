ac6a7f5f80a1bc98fff580720a45424867cc536a
sulochanadevianbuvel-bit <sulochanadevianbuvel@gmail.com>
2026-01-27 16:21:15 +0530

    Create playing_with_character.c

---
+++ playing_with_character.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch[100];
+    char s[100];
+    char sen[100];
+    fgets(ch, 100,stdin);
+    fgets(s, 100, stdin);  
+    fgets(sen, 100,stdin);
+    
+    printf("%s",ch);
+    printf("%s",s);
+    printf("%s",sen);
+
+    return 0;
+}
