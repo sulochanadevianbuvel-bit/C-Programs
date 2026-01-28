afca5d544e8bc23286d894df550dadcecd3c950e
sulochanadevianbuvel-bit <sulochanadevianbuvel@gmail.com>
2026-01-28 11:12:48 +0530

    Add files via upload

---
+++ problem1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+     scanf("%d",&n);
+    int size=2* n - 1;
+  	for(int i=0;i<size;i++){
+        for (int j=0; j<size; j++) {
+            int top=i;
+            int left=j;
+            int right=size-1-j;
+            int bottom=size-1-i;
+            
+            int min=top;
+            if(left<min) {
+                min=left;
+            }
+            if(right<min) {
+                min=right;
+            }
+            if(bottom<min) {
+                min=bottom;
+            }
+            int y=n-min;
+            printf("%d",y);
+        
+        }
+    printf("\n");
+    }
+    return 0;
+}
